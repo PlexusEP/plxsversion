6e47756bf6443056eb5abc33d51c4b6140752daa
Kevin Walker <kevin.walker@plexus.com>
2025-04-14 23:04:41 +0000

    feat: update util functions for better git support

--- src/version_builder/utils.py
+++ src/version_builder/utils.py
@@ -2,28 +2,18 @@ import os
 import subprocess
 
 
-class ChDir(object):
-    """
-    Execute a block of functionality in a particular directory
-
-    Usage:
-    with ChDir(my_dir):
-        do_something()
-    """
-
-    def __init__(self, directory):
-        self.directory = directory
-
-    def __enter__(self):
-        self.old_dir = os.getcwd()
-        os.chdir(self.directory)
-
-    def __exit__(self, exc_type, value, traceback):
-        os.chdir(self.old_dir)
+def change_dir(path):
+    original_dir = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(original_dir)
 
 
 class EqualityByValue(object):
     """Override identity eq with a check of the object's underlying fields"""
+
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.__dict__ == other.__dict__
@@ -34,31 +24,31 @@ class EqualityByValue(object):
 
 
 class Git(object):
-    def get_cwd_branch_name():
+    def get_branch_name():
         return subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip().decode()
 
-    def get_cwd_commit_id():
-        return subprocess.check_output(["git", "log", "--format=%h", "-n", "1"]).strip().decode()
+    def get_commit_id():
+        return subprocess.check_output(["git", "rev-parse", "--short=7", "HEAD"]).strip().decode()
 
-    def get_cwd_commit_count():
+    def get_description():
+        """Output format: <tag>-<commits_since_tag>-g<commit_hash_abbrev>"""
+        return subprocess.check_output(["git", "describe", "--tags", "--abbrev=7"]).strip().decode()
+
+    def get_commit_count():
         try:
-            with open(os.devnull, "w") as devnull:
-                return int(subprocess.check_output(["git", "rev-list", "HEAD", "--count"], stderr=devnull))
+            return int(subprocess.check_output(["git", "rev-list", "HEAD", "--count"]))
         except subprocess.CalledProcessError:
+            # HEAD likely does not exist, meaning no commits
             return 0
 
     def get_cwd_is_not_empty():
+        """Returns true if a directory contains files besides a .git directory"""
         all_entries = os.listdir(os.getcwd())
         nongit_entries = [entry for entry in all_entries if entry != ".git"]
         return len(nongit_entries) != 0
 
-    def get_cwd_contains_untracked_files():
-        return subprocess.check_output(["git", "ls-files", "--exclude-standard", "--others"]).strip().decode() != ""
-
-    def get_cwd_contains_modified_files():
-        # Usually we'd like to use "git diff-index" here.
-        # But there seems to be a bug that when we run "chmod 755 file" on a file that already has 755 and is committed to
-        # git as such, the next run of "git diff-index" will show it as a difference. "git diff" seams to work
-        return 0 != (subprocess.call(["git", "diff", "--exit-code", "--quiet", "HEAD"])) or (
-            0 != subprocess.call(["git", "diff", "--cached", "--exit-code", "--quiet", "HEAD"])
-        )
+    def get_is_dirty():
+        staged_changes = subprocess.call(["git", "diff", "--quiet", "--cached", "--exit-code", "HEAD"]) != 0
+        unstaged_changes = subprocess.call(["git", "diff", "--quiet", "--exit-code", "HEAD"]) != 0
+        untracked_files = subprocess.call(["git", "ls-files", "--exclude-standard", "--others"]) != ""
+        return staged_changes or unstaged_changes or untracked_files
