bfee1674e73fdfda83a8579dfc2f6f42fdedfa6f
Kevin Walker <kevin.walker@plexus.com>
2025-04-17 18:43:18 +0000

    feat: test main and fix bugs

--- src/version_builder/main.py
+++ src/version_builder/main.py
@@ -1,7 +1,14 @@
+from pathlib import PosixPath
+
 from version_builder import version_collector, formatter
 
 
-def get_version(source, source_input):
+def create_version_file(source, source_input, output_file, lang, print_created_file):
+    version_info = _get_version(source, source_input)
+    _output_version_file(version_info, PosixPath(output_file), lang, print_created_file)
+
+
+def _get_version(source, source_input):
     """Obtains version info from a particular data source"""
     match source:
         case "git":
@@ -12,26 +19,21 @@ def get_version(source, source_input):
             raise ValueError("Unknown source")
 
 
-def create_version_file(source, source_input, output_file, lang, print_created_file):
-    version_info = get_version(source, source_input)
-    _output_version_file(version_info, output_file, lang, print_created_file)
-
-
 def _output_version_file(version_info, output_file, lang, print_created_file):
     """Converts version info into a requested format and outputs to a file"""
     match lang:
         case "cpp":
             output = formatter.to_cpp(version_info)
-            file_extension = ".hpp"
+            expected_file_extension = ".hpp"
         case "c":
             output = formatter.to_c(version_info)
-            file_extension = ".h"
+            expected_file_extension = ".h"
         case _:
             raise ValueError("Unknown language")
 
-    if not output_file.endswith(file_extension):
+    if output_file.suffix != expected_file_extension:
         raise ValueError(
-            f"Unexpected file ending for lang {lang:s}. Expected: *{file_extension:s}. Got: {output_file:s}"
+            f"Unexpected file ending for lang {lang:s}. Expected: *{expected_file_extension:s}. Got: {output_file.name:s}"
         )
 
     with open(output_file, "w") as file:
