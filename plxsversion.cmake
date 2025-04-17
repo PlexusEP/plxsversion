a957dff66d950c3755660ef194b671aefb1e1e9b
Kevin Walker <kevin.walker@plexus.com>
2025-04-17 21:03:17 +0000

    feat: update cmake clean to remove this version file

--- plxsversion.cmake
+++ plxsversion.cmake
@@ -2,12 +2,12 @@ set(DIR_OF_PLXSVERSION "${CMAKE_CURRENT_LIST_DIR}" CACHE INTERNAL "DIR_OF_PLXSVE
 
 find_package(Python COMPONENTS Interpreter REQUIRED)
 
-macro(_set_file_extension LANG)
-  set(FILE_EXT "hpp")
+macro(_set_relative_out_file_path LANG)
+  set(REL_OUT_PATH "plxs/plxsversion/version.hpp")
   if(${LANG} STREQUAL "c")
-    set(FILE_EXT "h")
+    set(REL_OUT_PATH "plxs/plxsversion/version.h")
   endif()
-endmacro(_set_file_extension)
+endmacro(_set_relative_out_file_path)
 
 function(_create_version_file LANG SOURCE INPUT)
   cmake_parse_arguments(
@@ -23,7 +23,7 @@ function(_create_version_file LANG SOURCE INPUT)
 
   set(ENV{PYTHONPATH} "${DIR_OF_PLXSVERSION}/src:ENV{PYTHONPATH}")
   execute_process(
-    COMMAND /usr/bin/env ${Python_EXECUTABLE} -m version_builder --lang ${LANG} --source ${SOURCE} --input ${INPUT} ${CREATE_ADDITIONAL_OPTIONS} "${CMAKE_CURRENT_BINARY_DIR}/plxs/plxsversion/version.${FILE_EXT}"
+    COMMAND /usr/bin/env ${Python_EXECUTABLE} -m version_builder --lang ${LANG} --source ${SOURCE} --input ${INPUT} ${CREATE_ADDITIONAL_OPTIONS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_OUT_PATH}"
 		  RESULT_VARIABLE result)
   if(NOT ${result} EQUAL 0)
     message(FATAL_ERROR "Error running plxsversion tool. Return code is: ${result}")
@@ -32,7 +32,7 @@ endfunction(_create_version_file)
 
 # Load version string and write it to a cmake variable so it can be accessed from cmake.
 function(_set_version_cmake_variable OUTPUT_VARIABLE)
-  file(READ "${CMAKE_CURRENT_BINARY_DIR}/plxs/plxsversion/version.${FILE_EXT}" VERSION_FILE_CONTENT)
+  file(READ "${CMAKE_CURRENT_BINARY_DIR}/${REL_OUT_PATH}" VERSION_FILE_CONTENT)
   string(REGEX REPLACE ".*VERSION = \"([^\"]*)\".*" "\\1" VERSION "${VERSION_FILE_CONTENT}")
   message(STATUS "Version from plxsversion: ${VERSION}")
   set(${OUTPUT_VARIABLE} "${VERSION}" CACHE INTERNAL "${OUTPUT_VARIABLE}")
@@ -75,8 +75,9 @@ function(target_plxsversion_init TARGET)
     set(VER_INPUT ${CMAKE_CURRENT_SOURCE_DIR})
   endif()
 
-  _set_file_extension(${VER_LANG})
+  _set_relative_out_file_path(${VER_LANG})
   _create_version_file(${VER_LANG} ${VER_SOURCE} ${VER_INPUT} ADDITIONAL_OPTIONS ${OPTIONS})
   target_include_directories(${TARGET} PUBLIC "${CMAKE_CURRENT_BINARY_DIR}/plxs")
+  set_property(TARGET ${TARGET} APPEND PROPERTY ADDITIONAL_CLEAN_FILES "${CMAKE_CURRENT_BINARY_DIR}/${REL_OUT_PATH}")
   _set_version_cmake_variable(PLXSVERSION_STRING)
 endfunction(target_plxsversion_init)
